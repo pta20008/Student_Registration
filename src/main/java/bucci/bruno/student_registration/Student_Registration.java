00aa493f928da9bc5e0fce60ce38992e1b379f4b
Bruno Bucci Vieira <pta20008@student.cct.ie>
2024-01-30 20:27:53 -0300

    first commit

---
+++ src/main/java/bucci/bruno/student_registration/Student_Registration.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bucci.bruno.student_registration;
+
+/**
+ *
+ * @author Bruno Bucci
+ */
+public class Student_Registration {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
