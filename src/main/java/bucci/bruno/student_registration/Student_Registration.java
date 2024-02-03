fb38ff1d9e1105cf7c0fec274e548b8ffd40be71
Bruno Bucci Vieira <pta20008@student.cct.ie>
2024-02-03 14:11:22 -0300

    Adding new method, processStudentData for processing student data, reading files and others.

--- src/main/java/bucci/bruno/student_registration/Student_Registration.java
+++ src/main/java/bucci/bruno/student_registration/Student_Registration.java
@@ -4,14 +4,32 @@
 
 package bucci.bruno.student_registration;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
- *
  * @author Bruno Bucci
  */
 public class Student_Registration {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    processStudentData();
+  }
+
+  /**
+   * Method for processing student data, reading files and writing
+   **/
+  private static void processStudentData() {
+      try {
+          BufferedReader reader = new BufferedReader(new FileReader("students.txt"));
+          FileWriter writer = new FileWriter("status.txt");
+
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
+
 
 }
