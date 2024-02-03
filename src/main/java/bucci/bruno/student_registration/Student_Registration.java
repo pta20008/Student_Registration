1a7f4ab6fb596172b4e3a958c5a506c728d108df
Bruno Bucci Vieira <pta20008@student.cct.ie>
2024-02-03 14:16:15 -0300

    Complementing the processStudentData method and creating auxiliary methods for processing

--- src/main/java/bucci/bruno/student_registration/Student_Registration.java
+++ src/main/java/bucci/bruno/student_registration/Student_Registration.java
@@ -20,16 +20,66 @@ public class Student_Registration {
 
   /**
    * Method for processing student data, reading files and writing
-   **/
+   */
   private static void processStudentData() {
       try {
           BufferedReader reader = new BufferedReader(new FileReader("students.txt"));
           FileWriter writer = new FileWriter("status.txt");
 
+        String line;
+        while ((line = reader.readLine()) != null) {
+          String firstName = line;
+          String secondName = reader.readLine();
+          int numberOfClasses = Integer.parseInt(reader.readLine());
+          String studentNumber = reader.readLine();
+
+          if (isValidData(firstName, secondName, numberOfClasses, studentNumber)) {
+            writeToFile(writer, studentNumber, secondName, getWorkload(numberOfClasses));
+          } else {
+            System.out.println("Error: Invalid data for student - " + studentNumber);
+          }
+        }
+
+        reader.close();
+        writer.close();
       } catch (IOException e) {
-          e.printStackTrace();
+        e.printStackTrace();
       }
   }
 
+  /**
+   * Checks if the student data is valid according to the specified rules.
+   *
+   * @param firstName       The first name of the student.
+   * @param secondName      The second name of the student.
+   * @param numberOfClasses The number of classes the student is enrolled in.
+   * @param studentNumber   The student number.
+   * @return True if the data is valid, False otherwise.
+   */
+  private static boolean isValidData(String firstName, String secondName, int numberOfClasses, String studentNumber) {
+    return true;
+  }
+
+  /**
+   * Writes student data to a file in the specified format.
+   *
+   * @param writer        FileWriter object for writing to the file.
+   * @param studentNumber The student number.
+   * @param secondName    The second name of the student.
+   * @param workload      The workload determined by the number of classes.
+   * @throws IOException If an I/O error occurs while writing to the file.
+   */
+  private static void writeToFile(FileWriter writer, String studentNumber, String secondName, String workload) throws IOException {
+  }
+
+  /**
+   * Determines the workload based on the number of classes.
+   *
+   * @param numberOfClasses The number of classes for the student.
+   * @return A string representing the workload based on the specified criteria.
+   */
+  private static String getWorkload(int numberOfClasses) {
+    return "";
+  }
 
 }
